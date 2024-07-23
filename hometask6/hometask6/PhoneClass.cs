2d020930d0c26f96ee18a9ce11975462c9cc1f2d
dubatovka.maksim <dubatovvka.maksim@gmail.com>
2024-07-23 16:23:32 +0300

    Add hometask6

---
+++ hometask6/hometask6/PhoneClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hometask6
+{
+    public class PhoneClass
+    {
+        public string number = "58558484";
+        public string model = "model1";
+        public double weigth = 2.2;
+
+        public PhoneClass() { }
+        public PhoneClass(string number, string model, double weigth) 
+        {
+            this.number = number;
+            this.model = model;
+            this.weigth = weigth;
+        }
+        public PhoneClass (string number, string model) : this(number, model, 0)
+        {
+            this.number = number;
+            this.model = model;
+        }
+        public void phoneOutput()
+        {
+            Console.WriteLine($"Номер: {number}, модель: {model}");
+        }
+        public void receiveCall()
+        {
+            Console.WriteLine($"Звонит {number}");
+        }
+        public void receiveCall(string name)
+        {
+            Console.WriteLine($"Звонит {name}, {number}, {weigth}");
+        }
+        public string getNumber()
+        {
+            return number;
+        }
+        public void sendMessage(params string[] phoneNumbers)
+        {
+            foreach (var sendNumber in phoneNumbers)
+            {
+                Console.WriteLine($"Отправка сообщения на номер:{sendNumber} от {number}");
+            }
+        }
+    }
+}
---
+++ hometask6/hometask6/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace hometask6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneClass phone1 = new PhoneClass();
+            phone1.phoneOutput();
+            phone1.receiveCall("Неизвестный абонент");
+            phone1.receiveCall();
+            Console.WriteLine(phone1.getNumber());
+            phone1.sendMessage("5454545454");
+            PhoneClass phone2 = new PhoneClass("4548484", "Iphone", 2.4);
+            phone2.phoneOutput();
+            phone2.receiveCall("Nick");
+            phone2.receiveCall();
+            Console.WriteLine(phone2.getNumber());
+            phone2.sendMessage(phone1.getNumber());
+            PhoneClass phone3 = new PhoneClass("4848484848", "Samsung");
+            phone3.phoneOutput();
+            phone3.receiveCall("Pol");
+            phone3.receiveCall();
+            Console.WriteLine(phone3.getNumber());
+            phone3.sendMessage("34343434");
+        }
+    }
+}
